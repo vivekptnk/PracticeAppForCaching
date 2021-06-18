8c97d68ffa1a2a9890cc1b210995f68c65263ad5
vivekptnk <vivekpattanaik100@gmail.com>
2021-06-18 19:21:33 -0400

    Added photo models and by downloading from internet

--- PracticeAppForCaching/ViewModels/DownloadingImagesViewModel .swift
+++ PracticeAppForCaching/ViewModels/DownloadingImagesViewModel .swift
@@ -6,8 +6,21 @@
 //
 
 import Foundation
-
+import Combine
 class DownloadingImagesViewModel : ObservableObject {
     @Published  var dataArray : [PhotoModel] = []
+    var cancellables = Set<AnyCancellable>()
+    let dataService = PhotoModelDataService.instance
     
+    init() {
+        addSubscriber()
+    }
+    
+    func addSubscriber() {
+        dataService.$photoModels
+            .sink {[weak self] (returnedPhotoModels) in
+                self?.dataArray = returnedPhotoModels
+            }
+            .store(in: &cancellables )
+    }
 }
