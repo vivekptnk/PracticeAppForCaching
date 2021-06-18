f56e0f504dc9e3c92f0ec5766bc7ea885c1301c1
vivekptnk <vivekpattanaik100@gmail.com>
2021-06-18 18:53:07 -0400

    Initial Commit

---
+++ PracticeAppForCaching/PracticeAppForCachingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeAppForCachingApp.swift
+//  PracticeAppForCaching
+//
+//  Created by Vivek Pattanaik on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct PracticeAppForCachingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
